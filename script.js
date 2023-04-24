c9633df061c9b756f6c76894679540c2f1da9a21
SK. Edwards <125165568+SK-Edwards@users.noreply.github.com>
2023-04-24 19:15:37 -0400

    First fetch call. Trying to display data

--- script.js
+++ script.js
@@ -1,23 +1,59 @@
-var apiKey = 'b5b75a8f63cdacf8b0e3ede86eab14d8';
-var city = " ";
+var apiKey = 'd02d8e895a3cd1e28bb8edcd5100fe8b';
+var city = document.querySelector(".citySearchInput").value.trim() ;
 var citySearchBtn = document.querySelector(".citySearchBtn");
 var citySearchInput = document.querySelector(".citySearchInput");
 var previousSearches = document.querySelector(".previousSearches");
- 
-var requestUrl = "http://api.weatherstack.com/current?access_key=" + apiKey + "&query=" + citySearchInput.value.trim();
-    
+ var todaysForcast = document.querySelector('.todaysForcast')
+ var date = dayjs();
+ var todaysIcon = document.getElementById('todaysIcon')
+ var todaysIcon = document.querySelector("#todaysIcon");
+ var todaysTemp = document.querySelector("#todaysTemp");
+ var todaysWind = document.querySelector("#todaysWind");
+ var todaysHum = document.querySelector("#todaysHum");
+
 function cityForcastCall(){
+  var city = document.querySelector(".citySearchInput").value.trim() ;
+  var todaysForcast = document.querySelector('.todaysForcast')
+ var date = dayjs();
+
+
+  var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
+
     fetch(requestUrl)
-      .then(function (response) {
+      .then(function (response) {  
+        
+        console.log(response);
         return response.json();
+      
 
          })
      
       .then(function (data) {
         console.log(data);
-        console.log(citySearchInput.value.trim());
-        });
-    
+        console.log(data.name);
+        console.log(data.wind.speed);
+        console.log(data. main.temp);
+        console.log(data.main.humidity);
+
+
+
+
+        todaysForcast.textContent = data.name + " " + date.format('(M/D/YYYY)');
+            
+        
+            var icon = data.weather[0].icon;
+
+            var todaysIcon = document.querySelector("#todaysIcon");
+            var todaysTemp = document.querySelector("#todaysTemp");
+            var todaysWind = document.querySelector("#todaysWind");
+            var todaysHum = document.querySelector("#todaysHum");
+            var iconURL = "https://openweathermap.org/img/w/" + icon + ".png";
+
+           todaysIcon.setAttribute("src", iconURL);
+        todaysTemp.textContent = "Temp: " + data.main.temp + "°";
+        todaysWind.textContent = "Wind: " + data.wind.speed + " MPH";
+        todaysHum.textContent = "Humidity: " + data.main.humidity + "%";
+      });    
     }
   
 
